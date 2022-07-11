ce18a87212f7024320d82aefbea31d33a91ce6c2
MuMuXiLv <M202072857@hust.edu.cn>
2022-07-12 01:56:57 +0800

    'Test1'

--- src/main/java/com/nowcoder/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -30,8 +30,8 @@ public class HomeController {
         // 方法调用钱,SpringMVC会自动实例化Model和Page,并将Page注入Model.
         // 所以,在thymeleaf中可以直接访问Page对象中的数据.
         page.setRows(discussPostService.findDiscussPostRows(0));
-        page.setPath("/index");
-
+        page.setPath("/index"); //设置了pagepath 在index.html中会在点击网页下方的1 2 3 4 页码时，发送/index，同时还包括current
+        //信息，从而分页显示
         List<DiscussPost> list = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
         List<Map<String, Object>> discussPosts = new ArrayList<>();
         if (list != null) {
